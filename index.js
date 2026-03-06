bde4b664f62391ea0d3db4d66ba828dd4eaf2df6
amercado1199 <amercado1199@gmail.com>
2026-03-05 23:31:14 -0500

    Completed JS functions lab

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+function calculateTax(amount) {
+    return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+function findMaximum(num1 , num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+function isPalindrome(word) {
+    let reversed = word.split("").reverse().join("");
+    return word === reversed;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    let discount = originalPrice * (discountPercentage / 100);
+    return originalPrice - discount;
+}
 
 
 
